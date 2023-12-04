7dc050232c43be287646883bdb29b299c404b1b8
Leticia Faria <142911045+letsay@users.noreply.github.com>
2023-12-04 01:45:00 -0300

    Add files via upload

--- js/jogo-puzzle.js
+++ js/jogo-puzzle.js
@@ -1,86 +1,84 @@
-var rows = 3;
-var columns = 3;
+const imageUrl = 'url("../images/jogo-puzzle/foto-puzzle.jpg")';
+const pieces = 3;
+const pieceSize = 100;
+let emptyPiece = { row: pieces - 1, col: pieces - 1 };
 
-var carrTile;
-var otherTile; //em branco
+function createPuzzlePiece(row, col) {
+    const piece = document.createElement('div');
+    piece.classList.add('puzzle-piece');
+    piece.style.backgroundImage = imageUrl;
+    piece.style.backgroundPosition = `-${col * pieceSize}px -${row * pieceSize}px`;
+    piece.dataset.row = row;
+    piece.dataset.col = col;
+    piece.addEventListener('click', () => movePiece(row, col));
+    return piece;
+}
 
-var turns = 0;
+function shufflePuzzle() {
+    const puzzlePieces = document.querySelectorAll('.puzzle-piece');
+    puzzlePieces.forEach(piece => {
+        const randomRow = Math.floor(Math.random() * pieces);
+        const randomCol = Math.floor(Math.random() * pieces);
+        piece.style.gridRow = randomRow + 1;
+        piece.style.gridColumn = randomCol + 1;
+        piece.dataset.row = randomRow;
+        piece.dataset.col = randomCol;
+    });
+}
 
-//var imgOrder = ["1","2","3","4","5","6","7","8","9"]; ordem normal
+function movePiece(row, col) {
+    if (isAdjacent(row, col, emptyPiece.row, emptyPiece.col)) {
+        swapPieces(row, col, emptyPiece.row, emptyPiece.col);
+        emptyPiece = { row, col };
+        checkWin();
+    }
+}
 
-var imgOrder = ["4","2","8","5","1","6","7","9","3"];
+function isAdjacent(row1, col1, row2, col2) {
+    return Math.abs(row1 - row2) + Math.abs(col1 - col2) === 1;
+}
 
-window.onload = function(){
-    for (let r=0; r < rows; r++){
-        for (let c=0; c < columns; c++){
-            //<img>
-            let tile = document.createElement("img");
-            tile.id = r.toString() + c.toString();
-            tile.src = imgOrder.shift()+ ".png" ;
+function swapPieces(row1, col1, row2, col2) {
+    const piece1 = document.querySelector(`[data-row="${row1}"][data-col="${col1}"]`);
+    const piece2 = document.querySelector(`[data-row="${row2}"][data-col="${col2}"]`);
 
-            //funções drag/arrasta
-            tile.addEventListener("dragstart", dragStart);
-            tile.addEventListener("dragover", dragOver);
-            tile.addEventListener("dragenter", dragEnter);
-            tile.addEventListener("dragleave", dragLeave);
-            tile.addEventListener("drop", dragDrop);
-            tile.addEventListener("dragend",dragEnd);
+    const tempRow = piece1.style.gridRow;
+    const tempCol = piece1.style.gridColumn;
 
-            document.getElementById("board").append(tile);
+    piece1.style.gridRow = piece2.style.gridRow;
+    piece1.style.gridColumn = piece2.style.gridColumn;
+    piece2.style.gridRow = tempRow;
+    piece2.style.gridColumn = tempCol;
+
+    [piece1.dataset.row, piece2.dataset.row] = [piece2.dataset.row, piece1.dataset.row];
+    [piece1.dataset.col, piece2.dataset.col] = [piece2.dataset.col, piece1.dataset.col];
+}
+
+function checkWin() {
+    const puzzlePieces = document.querySelectorAll('.puzzle-piece');
+    const isSolved = Array.from(puzzlePieces).every(piece => {
+        const correctRow = parseInt(piece.dataset.row);
+        const correctCol = parseInt(piece.dataset.col);
+        return piece.style.gridRow === `${correctRow + 1}` && piece.style.gridColumn === `${correctCol + 1}`;
+    });
+
+    if (isSolved) {
+        alert('Parabéns! Você venceu!');
+        shufflePuzzle(); // Embaralha o quebra-cabeça novamente após resolvido
+    }
+}
+
+function createPuzzle() {
+    const puzzleContainer = document.getElementById('puzzle-container');
+
+    for (let row = 0; row < pieces; row++) {
+        for (let col = 0; col < pieces; col++) {
+            const piece = createPuzzlePiece(row, col);
+            puzzleContainer.appendChild(piece);
         }
     }
+
+    shufflePuzzle();
 }
 
-function dragStart(){
-    currTile = this;
-}
-
-function dragOver(e) {
-    e.preventDefault();
-}
-
-function dragEnter(e) {
-    e.preventDefault();
-}
-
-function dragLeave() {
-}
-
-function dragDrop() {
-    otherTile = this; //foto sendo solta (dropped)
-}
-
-function dragEnd() {
-    if (!otherTile.src.includes("3.jpg")) {
-        return;
-    }
-
-    let currCoords = currTile.id.split("-"); // "0-0" -> ["0", "0"]
-    let r = parseInt(currCoords[0]);
-    let c = parseInt(currCoords[1]);
-
-    let otherCoords = otherTile.id.split("-");
-    let r2 = parseInt(otherCoords[0]);
-    let c2 = parseInt(otherCoords[1]);
-
-    let moveLeft = r == r2 && c2 == c-1;
-    let moveRight = r == r2 && c2 == c+1;
-
-    let moveUp = c == c2 && r2 == r-1;
-    let moveDown = c == c2 && r2 == r+1;
-
-    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
-
-    if (isAdjacent) {
-        let currImg = currTile.src;
-        let otherImg = otherTile.src;
-
-        currTile.src = otherImg;
-        otherTile.src = currImg;
-
-        turns += 1;
-        document.getElementById("turns").innerText = turns;
-    }
-
-
-}
+window.onload = createPuzzle;
